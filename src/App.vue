2c0bb6de122360a6bd995126ec49a77115e171a9
Pa OaTz InDy <oatkung2541@gmail.com>
2021-08-06 20:12:13 +0700

    .

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <img :src="require('./assets/images/CHULA @1X.png')" />
+</template>
+
+<script>
+// import { ref } from 'vue'
+export default {
+    name: 'App',
+    components: {},
+    setup() {},
+}
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+body {
+    background-image: url('./assets/images/Group 101@3X.png');
+}
+</style>
