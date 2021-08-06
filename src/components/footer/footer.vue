9bc28de53669162495ee329150c3a8545bf5910d
Pa OaTz InDy <oatkung2541@gmail.com>
2021-08-06 23:42:12 +0700

    .

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="footer">footer</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+    background-image: url('../../assets/images/home/footer/ve@2X.png');
+}
+</style>
---
+++ src/layouts/Default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>layout</div>
+    <router-view />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
